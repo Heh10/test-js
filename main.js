173d5fd3b3650ec52e911af973be774a014279b4
he <1021457010@qq.com>
2017-03-24 15:12:48 +0800

    111

---
+++ main.js
@@ -0,0 +1,9 @@
+/**
+ * Created by HE on 2017/3/24.
+ */
+function a() {
+    var b = document.getElementById("b");
+    b.style.height = "300px";
+    b.style.width = "300px";
+    b.style.backgroundColor = "red";
+}
